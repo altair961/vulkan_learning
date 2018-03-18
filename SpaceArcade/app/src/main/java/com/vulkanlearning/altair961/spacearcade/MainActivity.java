90e5629aafc294af2715ab19f04e604e2e652a1c
altair961 <altair961@example.com>
2018-03-18 22:33:00 +0300

    Added a project with empty activity named SpaceArcade

---
+++ SpaceArcade/app/src/main/java/com/vulkanlearning/altair961/spacearcade/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vulkanlearning.altair961.spacearcade;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+////////////////////////////////////////////////////////////////////////////////
+// The class outlines the Space Arcade native activity. 2018/03/18
+public class MainActivity extends NativeActivity
+{
+    /////////////////////////////////////////////////////////////////////////////
+// The activity's on create. 2018/03/18
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    } // End of public void onCreate(Bundle savedInstanceState)
+} // End of public class MainActivity extends NativeActivity

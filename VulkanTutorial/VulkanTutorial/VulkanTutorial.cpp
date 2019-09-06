5615c24fec69d95c3e9828cdb5494bb0e54e33be
altair961 <altair961@users.noreply.github.com>
2019-09-06 22:31:12 +0300

    Defined VkInstanceCreateInfo

--- VulkanTutorial/VulkanTutorial/VulkanTutorial.cpp
+++ VulkanTutorial/VulkanTutorial/VulkanTutorial.cpp
@@ -15,5 +15,15 @@ int main()
 	appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 0);
 	appInfo.apiVersion = VK_VERSION_1_1;
 
+	VkInstanceCreateInfo instanceInfo;
+	instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceInfo.pNext = NULL;
+	instanceInfo.flags = 0;
+	instanceInfo.pApplicationInfo = &appInfo;
+	instanceInfo.enabledLayerCount = 0;
+	instanceInfo.ppEnabledLayerNames = NULL;
+	instanceInfo.enabledExtensionCount = 0;
+	instanceInfo.ppEnabledExtensionNames = NULL;
+
 	return 0;
 }

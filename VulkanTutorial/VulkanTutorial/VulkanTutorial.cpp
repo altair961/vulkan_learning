1cc34a12cd54df586af93a1859b300e2fd419efc
altair961 <altair89@list.ru>
2020-01-14 21:15:53 +0300

    Created appInfo structure with basic information of the application

--- VulkanTutorial/VulkanTutorial/VulkanTutorial.cpp
+++ VulkanTutorial/VulkanTutorial/VulkanTutorial.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "vulkan/vulkan.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pNext = NULL;
+    appInfo.pApplicationName = "Vulkan Tutorial";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "Super Vulkan Engine Turbo Mega";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_1;
+
+    return 0;
 }

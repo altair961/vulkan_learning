aba7f48d6a77de0fe064f1b20549018f475d4d99
altair961 <altair89@list.ru>
2020-01-24 21:31:20 +0300

    Got physical device available features

--- VulkanTutorialNix/VulkanTutorial.cpp
+++ VulkanTutorialNix/VulkanTutorial.cpp
@@ -9,20 +9,24 @@
 VkInstance instance;
 
 void printStats(VkPhysicalDevice &device) {
-    VkPhysicalDeviceProperties properties;
-    vkGetPhysicalDeviceProperties(device, &properties);
+	VkPhysicalDeviceProperties properties;
+	vkGetPhysicalDeviceProperties(device, &properties);
 
-    std::cout << "Name: " << properties.deviceName << std::endl;
-    uint32_t apiVer = properties.apiVersion;
-    std::cout << "API Version " << VK_VERSION_MAJOR(apiVer) << "."
-                                << VK_VERSION_MINOR(apiVer) << "."
-                                << VK_VERSION_PATCH(apiVer) << std::endl;
-    std::cout << "Driver Version " << properties.driverVersion << std::endl;
-    std::cout << "Vendor ID: " << properties.vendorID << std::endl;
-    std::cout << "Device ID: " << properties.deviceID << std::endl;
-    std::cout << "Device Type: " << properties.deviceType << std::endl;
+	std::cout << "Name: " << properties.deviceName << std::endl;
+	uint32_t apiVer = properties.apiVersion;
+	std::cout << "API Version " << VK_VERSION_MAJOR(apiVer) << "."
+                                    << VK_VERSION_MINOR(apiVer) << "."
+                                    << VK_VERSION_PATCH(apiVer) << std::endl;
+	std::cout << "Driver Version " << properties.driverVersion << std::endl;
+	std::cout << "Vendor ID: " << properties.vendorID << std::endl;
+	std::cout << "Device ID: " << properties.deviceID << std::endl;
+	std::cout << "Device Type: " << properties.deviceType << std::endl;
 
-    std::cout << std::endl;
+	VkPhysicalDeviceFeatures features;
+	vkGetPhysicalDeviceFeatures(device, &features);
+	std::cout << "Geometry Shader: " << features.geometryShader << std::endl;
+
+	std::cout << std::endl;
 }
 
 int main()

b3cc924a6cc17120bf275b13ea13f00c873be637
altair961 <altair89@list.ru>
2023-09-12 09:16:16 +0300

    Libraries paths were adjusted. Vulkan instance was created.

--- VulkankTutorial2/VulkanTutorial/VulkanTutorial.cpp
+++ VulkankTutorial2/VulkanTutorial/VulkanTutorial.cpp
@@ -1,4 +1,6 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 
 #include <iostream>
 #include <stdexcept>
@@ -6,33 +8,63 @@
 
 class HelloTriangleApplication {
 public:
-    void run() {
-    initVulkan();
-    mainLoop();
-    cleanup();
-}
+	void run()
+	{
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
 private:
-    void initVulkan() {
-    }
-    
-    void mainLoop() {
-    }
-    
-    void cleanup() {
-    }
+	void initVulkan() 
+	{
+		createInstance();
+	}
+
+	void mainLoop() {
+	}
+
+	void cleanup() {
+	}
+
+	VkInstance instance;
+
+	void createInstance() 
+	{
+		VkApplicationInfo appInfo {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) 
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
 };
 
 int main() {
-    HelloTriangleApplication app;
-    
-    try {
-        app.run();
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE; 
-    }
+	HelloTriangleApplication app;
 
-    return EXIT_SUCCESS;
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }

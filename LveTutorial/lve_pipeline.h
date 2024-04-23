274ccd4f4dfbaa618bc1cfe747677b9d138fd38e
altair961 <altair89@list.ru>
2024-04-23 07:48:50 +0300

    Graphics pipeline creation and configuration were implemented. Pipeline layout and render pass are yet to be defined. Assertions for pipelineLayout and renderPass were added.

--- LveTutorial/lve_pipeline.h
+++ LveTutorial/lve_pipeline.h
@@ -4,7 +4,20 @@
 #include "lve_device.h"
 
 namespace lve {
-	struct PipelineConfigInfo {};
+	struct PipelineConfigInfo {
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 	class LvePipeline {
 	public:
 		LvePipeline(
@@ -12,7 +25,7 @@ namespace lve {
 			const std::string& vertFilepath,
 			const std::string& fragFilepath,
 			const PipelineConfigInfo configInfo);
-		~LvePipeline() {}
+		~LvePipeline();
 
 		LvePipeline(const LvePipeline&) = delete;
 		void operator=(const LvePipeline&) = delete;
